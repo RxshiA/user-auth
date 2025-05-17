649843a124fd35ee8d9bd7651f994f18f5ae7a5c
RxshiA <rashiamarasiri@gmail.com>
2025-05-18 00:54:34 +0530

    refactor: Remove timestamp columns from User model

--- app/models/user.py
+++ app/models/user.py
@@ -6,9 +6,6 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
-    created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(
-        db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
 
     def __init__(self, username, password):
         self.username = username
